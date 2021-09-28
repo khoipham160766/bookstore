b5de4bf2fbb4fd9069b6e56704b922325e662e47
Phạm Trần Khôi <82496203+khoipham160766@users.noreply.github.com>
2021-09-28 20:03:20 +0700

    Update ChiTietChuongTrinhGiamGiaDTO.java

--- ManageBookStore/src/DTO/ChiTietChuongTrinhGiamGiaDTO.java
+++ ManageBookStore/src/DTO/ChiTietChuongTrinhGiamGiaDTO.java
@@ -47,7 +47,5 @@ public class ChiTietChuongTrinhGiamGiaDTO {
         this.MaSach = MaSach;
     }
     
-    public static void main (String []args){
-        System.out.print("aaaabbbbccccddddeeeee");
-    }
+    
 }
